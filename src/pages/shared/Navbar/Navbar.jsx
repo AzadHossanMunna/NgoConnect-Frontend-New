9a407fb7668bf57a528553cbe32a26ed12b70200
Azad Hossan Munna <azadhossanmunna1209@gmail.com>
2026-01-22 00:40:52 +0600

    Initial commit for new repo

--- src/pages/shared/Navbar/Navbar.jsx
+++ src/pages/shared/Navbar/Navbar.jsx
@@ -115,7 +115,7 @@ const Navbar = () => {
 
   </div>
   <div className="navbar-center hidden lg:flex">
-    <ul className="menu menu-horizontal px-1">
+    <ul className="menu menu-horizontal px-1 gap-4">
       {navItems}
     </ul>
   </div>
